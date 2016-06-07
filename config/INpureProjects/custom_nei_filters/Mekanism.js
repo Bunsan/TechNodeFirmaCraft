301e0e31d4aea0f08d58830cab0cb1e79b42d1c5
Bunsan <bunsan@users.noreply.github.com>
2016-06-07 10:44:28 -0500

    Fixed to display tiered Fluid tanks. Fixed a few config issues and made pre-filled gas and fluid tanks not display.

--- config/INpureProjects/custom_nei_filters/Mekanism.js
+++ config/INpureProjects/custom_nei_filters/Mekanism.js
@@ -1,5 +1,4 @@
 if (FML.isModLoaded("Mekanism")) {
-    NEI.override("Mekanism:MachineBlock2", [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]);
     NEI.override("Mekanism:*PlasticBlock", [0]);
     NEI.override("Mekanism:Balloon", [0]);
     NEI.override("Mekanism:GlowPanel", [0]);
